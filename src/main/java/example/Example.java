0b58912a06dabea9b3e9945b68fe634f58f5d43a
grave <grave.coder@gmail.com>
2014-03-09 22:45:14 +0800

    example

---
+++ src/main/java/example/Example.java
@@ -0,0 +1,12 @@
+package example;
+
+/**
+ * Created by grave on 14-3-9.
+ */
+public class Example {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
